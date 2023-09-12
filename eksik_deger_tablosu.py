8d57cc781b9c4aa67530c702e2fb7063d34f5f21
ilyas_demir <80126067+ilyasdemir-demirilyas@users.noreply.github.com>
2023-09-12 22:25:52 +0300

    Update eksik_deger_tablosu.py

--- eksik_deger_tablosu.py
+++ eksik_deger_tablosu.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from tabulate import tabulate
+
 def eksik_deger_tablosu(df):
     eksik_deger = df.isnull().sum()
     eksik_deger_yuzde = 100 * df.isnull().sum() / len(df)
@@ -6,4 +8,14 @@ def eksik_deger_tablosu(df):
     eksik_deger_tablo_son = eksik_deger_tablo.rename(
         columns={0: 'Eksik Değerler', 1: '% Değeri'})
     eksik_deger_tablo_son["Veri Tipi"] = df.dtypes
+    
+    # Benzersiz değer sayılarını ekleyin
+    benzersiz_degerler = df.nunique()
+    eksik_deger_tablo_son["Benzersiz Değerler"] = benzersiz_degerler
+    
     return eksik_deger_tablo_son
+
+eksik_deger_tablo = eksik_deger_tablosu(df_train)
+
+# Tabloyu düzenli bir şekilde görüntüleyin
+print(tabulate(eksik_deger_tablo, headers='keys', tablefmt='pretty'))
